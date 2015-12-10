53cef914c4d9ec9e8d48439c054cfed26d87149e
Peter Olah1 <peter_olah1@epam.com>
2015-12-10 14:33:57 +0100

    Driver class created and added to selenium branch under the util package.

---
+++ example-tests/src/test/java/com/epam/restassured/util/Driver.java
@@ -0,0 +1,40 @@
+package com.epam.restassured.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Peter_Olah1 on 12/10/2015.
+ */
+public class Driver {
+    private static WebDriver driver;
+
+    /**
+     * Class constructor.
+     */
+    static {
+        if (driver == null)
+        {
+            driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * @return the {@link WebDriver}.
+     */
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    /**
+     *  Quits the driver. Closes every associated window.
+     */
+    public static void tearDown() {
+        if (Driver.getDriver() != null) {
+            Driver.getDriver().quit();
+        }
+    }
+}
