ce4f0b9c697247c8df747a3cc1c547933e9dafe8
Peter Olah1 <peter_olah1@epam.com>
2015-12-10 14:50:00 +0100

    Refactored addRecord() method for use WebDriver to execute a subsrciption. After method added to close browser after the subscription is finished.

--- example-tests/src/test/java/com/epam/restassured/BasicServiceTest.java
+++ example-tests/src/test/java/com/epam/restassured/BasicServiceTest.java
@@ -7,59 +7,77 @@ package com.epam.restassured;
 //TODO: 5. create DDT script for REST script
 //TODO: 6. create DDT script for webdriver script
 //TODO: 7. create rest script without BDD style (using JUnit asssertions)
+
+import com.epam.restassured.pageobjects.HomePageObject;
+import com.epam.restassured.pageobjects.ThankYouPageObject;
+import com.epam.restassured.util.Driver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-
 public class BasicServiceTest {
-	//Test data
-	private String firstName;
-	private String lastName;
-	private String emailAddress;
-	private String emailAddressConfirmation;
-	private String newsletterOptIn;
+    // Test data
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String emailAddressConfirmation;
+    private boolean subscribeNewsletter;
 
-	//Verification
-	private List<String> listToVerifyEmail;
-	private static final int NUMBER_OF_RESPONSE = 1;
-	private static final String CONTENT_NUMBER_OF_ELEMENTS = "numberOfElements";
-	private static final String CONTENT_EMAIL_ADDRESS = "content.emailAddress";
-	private static final int HTTP_OK = 200;
+    // Verification
+    private List<String> listToVerifyEmail;
+    private static final int NUMBER_OF_RESPONSE = 1;
+    private static final String CONTENT_NUMBER_OF_ELEMENTS = "numberOfElements";
+    private static final String CONTENT_EMAIL_ADDRESS = "content.emailAddress";
+    private static final int HTTP_OK = 200;
+    private HomePageObject homePageObject;
+    private ThankYouPageObject thankYouPageObject;
 
-	@Before
-	public void setUp() throws Exception {
-		//Delete all existing record
-		given().delete(ServiceTestingProperties.REST_API_URL);
-		
-		//Setup test data
-		firstName = "John";
-		lastName = "Doe";
-		emailAddress = "johndoe@freecloud.com";
-		emailAddressConfirmation = "johndoe@freecloud.com";
-		newsletterOptIn = "true";
-		
-		//Setup verification data
-		listToVerifyEmail = new ArrayList<String>();
-		listToVerifyEmail.add(emailAddress);
-	}
+    @Before
+    public void setUp() throws Exception {
+        // Delete all existing record
+        given().delete(ServiceTestingProperties.REST_API_URL);
 
-	@Test
-	public void addRecord() {
-		given().contentType(ServiceTestingProperties.JSON_CONTENT_TYPE).
-		and().post(ServiceTestingProperties.getUrlToPostData(firstName, lastName, emailAddress, emailAddressConfirmation, newsletterOptIn));
-		
-		when().get(ServiceTestingProperties.REST_API_URL).
-		
-		then().statusCode(HTTP_OK).
-		and().content(CONTENT_NUMBER_OF_ELEMENTS, is(NUMBER_OF_RESPONSE)).
-		and().content(CONTENT_EMAIL_ADDRESS, equalTo(listToVerifyEmail));
-	}
+        // Setup test data
+        firstName = "John";
+        lastName = "Doe";
+        emailAddress = "johndoe@freecloud.com";
+        emailAddressConfirmation = "johndoe@freecloud.com";
+        subscribeNewsletter = true;
+
+        // Setup verification data
+        listToVerifyEmail = new ArrayList<String>();
+        listToVerifyEmail.add(emailAddress);
+        Driver.getDriver().get("https://t7-f0x.rhcloud.com/subscription/subscription.html");
+        homePageObject = new HomePageObject(Driver.getDriver());
+    }
+
+    @Test
+    public void addRecord() throws Exception {
+        //given
+        thankYouPageObject = homePageObject.givenSubcribeToNewsletter(firstName, lastName, emailAddress, emailAddressConfirmation, subscribeNewsletter);
+        //when
+        thankYouPageObject.whenSubscribeFinishedCheckDataOnPage(firstName, emailAddress);
+        //when
+         when().get(ServiceTestingProperties.REST_API_URL).
+         //then
+         then().statusCode(HTTP_OK).
+         //and
+         and().content(CONTENT_NUMBER_OF_ELEMENTS, is(NUMBER_OF_RESPONSE)).
+         //and
+         and().content(CONTENT_EMAIL_ADDRESS, equalTo(listToVerifyEmail));
+    }
+
+    @After
+    public void tearDown(){
+        Driver.tearDown();
+    }
 
 }
