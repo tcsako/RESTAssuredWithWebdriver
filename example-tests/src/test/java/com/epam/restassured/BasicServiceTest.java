074a6aa1e1f009b33b824630d77f0aad30b2787d
Peter Olah1 <peter_olah1@epam.com>
2015-12-11 18:33:13 +0100

    -  Logger configured - Constructor modified - After class modified

--- example-tests/src/test/java/com/epam/restassured/BasicServiceTest.java
+++ example-tests/src/test/java/com/epam/restassured/BasicServiceTest.java
@@ -10,10 +10,12 @@ package com.epam.restassured;
 
 import com.epam.restassured.pageobjects.HomePageObject;
 import com.epam.restassured.pageobjects.ThankYouPageObject;
-import com.epam.restassured.util.Driver;
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +25,16 @@ import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * Created by Tamas_Csako
+ */
 public class BasicServiceTest {
+    private static Logger log = Logger.getLogger(BasicServiceTest.class.getName());
+    private static final int NUMBER_OF_RESPONSE = 1;
+    private static final String CONTENT_NUMBER_OF_ELEMENTS = "numberOfElements";
+    private static final String CONTENT_EMAIL_ADDRESS = "content.emailAddress";
+    private static final int HTTP_OK = 200;
+
     // Test data
     private String firstName;
     private String lastName;
@@ -33,51 +44,67 @@ public class BasicServiceTest {
 
     // Verification
     private List<String> listToVerifyEmail;
-    private static final int NUMBER_OF_RESPONSE = 1;
-    private static final String CONTENT_NUMBER_OF_ELEMENTS = "numberOfElements";
-    private static final String CONTENT_EMAIL_ADDRESS = "content.emailAddress";
-    private static final int HTTP_OK = 200;
     private HomePageObject homePageObject;
     private ThankYouPageObject thankYouPageObject;
+    private ThankYouPageVerifier thankYouPageVerifier;
+    private WebDriver driver;
 
+    /**
+     * Responsible for setting up test data and environment.
+     *
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
-        // Delete all existing record
+
+        log.info("*******************************************");
+        log.info("Deleting existing records");
         given().delete(ServiceTestingProperties.REST_API_URL);
 
-        // Setup test data
+        log.info("Setting up test data");
         firstName = "John";
         lastName = "Doe";
         emailAddress = "johndoe@freecloud.com";
         emailAddressConfirmation = "johndoe@freecloud.com";
         subscribeNewsletter = true;
 
-        // Setup verification data
+        log.info("Setting up verification data");
         listToVerifyEmail = new ArrayList<String>();
         listToVerifyEmail.add(emailAddress);
-        Driver.getDriver().get("https://t7-f0x.rhcloud.com/subscription/subscription.html");
-        homePageObject = new HomePageObject(Driver.getDriver());
+        log.info("Initializing Firefox driver");
+        driver = new FirefoxDriver();
+        log.info("Opening subscription page");
+        driver.get("https://t7-f0x.rhcloud.com/subscription/subscription.html");
+        homePageObject = new HomePageObject(driver);
+        thankYouPageVerifier = new ThankYouPageVerifier(driver);
     }
 
+    /**
+     * Makes an automated subscription then checks the data correctness.
+     *
+     * @throws Exception
+     */
     @Test
     public void addRecord() throws Exception {
-        //given
-        thankYouPageObject = homePageObject.givenSubcribeToNewsletter(firstName, lastName, emailAddress, emailAddressConfirmation, subscribeNewsletter);
-        //when
-        thankYouPageObject.whenSubscribeFinishedCheckDataOnPage(firstName, emailAddress);
-        //when
-         when().get(ServiceTestingProperties.REST_API_URL).
-         //then
-         then().statusCode(HTTP_OK).
-         //and
-         and().content(CONTENT_NUMBER_OF_ELEMENTS, is(NUMBER_OF_RESPONSE)).
-         //and
-         and().content(CONTENT_EMAIL_ADDRESS, equalTo(listToVerifyEmail));
+        log.info("Filling fields and sending data");
+        thankYouPageObject = homePageObject.givenSignUp(firstName, lastName, emailAddress, emailAddressConfirmation,
+                subscribeNewsletter);
+        log.info("Checking 'Thank you' page URL, subscriber name and e-mail");
+        thankYouPageVerifier.whenSubscribeFinishedCheckDataOnPage(firstName, emailAddress);
+        when().get(ServiceTestingProperties.REST_API_URL).
+                then().statusCode(HTTP_OK).
+                and().content(CONTENT_NUMBER_OF_ELEMENTS, is(NUMBER_OF_RESPONSE)).
+                and().content(CONTENT_EMAIL_ADDRESS, equalTo(listToVerifyEmail));
     }
 
+    /**
+     *  Closes the browser after test execution.
+     */
     @After
-    public void tearDown(){
-        Driver.tearDown();
+    public void closeDriver() {
+        log.info("Closing browser");
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
