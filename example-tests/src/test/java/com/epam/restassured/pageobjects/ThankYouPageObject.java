53dc95932968ce5a1baf5383648ae912949c7d8f
Peter Olah1 <peter_olah1@epam.com>
2015-12-11 17:33:52 +0100

    - Verifier methods are in separate class as requested - Constructor modified

--- example-tests/src/test/java/com/epam/restassured/pageobjects/ThankYouPageObject.java
+++ example-tests/src/test/java/com/epam/restassured/pageobjects/ThankYouPageObject.java
@@ -1,18 +1,17 @@
 package com.epam.restassured.pageobjects;
 
-import com.epam.restassured.util.Driver;
-
-
+import com.epam.restassured.util.AbstractDriver;
+import com.google.common.base.Verify;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import com.google.common.base.Verify;
 
 /**
+ * Contains page elements and interaction methods for the test class.
+ *
  * Created by Peter_Olah1 on 12/10/2015.
  */
-public class ThankYouPageObject {
+public class ThankYouPageObject extends AbstractDriver {
 
     @FindBy(css = "h1")
     private WebElement headerTitle;
@@ -26,21 +25,37 @@ public class ThankYouPageObject {
      * @param driver {@link WebDriver}.
      */
     public ThankYouPageObject(WebDriver driver) {
-        PageFactory.initElements(Driver.getDriver(), this);
+        super(driver);
+    }
+
+    /**
+     * @return HeaderSubtitle.
+     */
+    public WebElement getHeaderSubtitle() {
+        return headerSubtitle;
+    }
+
+    /**
+     * @return HeaderTitle.
+     */
+    public WebElement getHeaderTitle() {
+        return headerTitle;
     }
 
     /**
      * Verifies that is the page sub header containing the user's email address.
+     *
+     * @param firstName The displayed first name.
+     * @param email The displayed email address.
      */
     public void whenSubscribeFinishedCheckDataOnPage(String firstName, String email) {
-        Verify.verify(Driver.getDriver().getCurrentUrl().toString()
-                .startsWith("https://t7-f0x.rhcloud.com/subscription/thank-you.html?"));
+        Verify.verify(getDriver().getCurrentUrl().startsWith("https://t7-f0x.rhcloud.com/subscription/thank-you.html?"));
         isHeaderContainsFirstName(firstName);
         isSubTitleContainsEmail(email);
     }
 
     /**
-     *  Verifies that is the page header containing the user's first name.
+     * Verifies that is the page header containing the user's first name.
      *
      * @param firstName The user's first name.
      */
@@ -50,12 +65,12 @@ public class ThankYouPageObject {
     }
 
     /**
-     *  Verifies that is the page sub header contains the user's email address.
+     * Verifies that is the page sub header contains the user's email address.
      *
      * @param email The user's email address.
      *
      */
     private void isSubTitleContainsEmail(String email) {
-        Verify.verify(headerSubtitle.getText().toString().contains(email));
+        Verify.verify(headerSubtitle.getText().contains(email));
     }
 }
