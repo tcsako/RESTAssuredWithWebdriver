c97a6e6c952dc9e8e5e12234d756a51d711459e4
Peter Olah1 <peter_olah1@epam.com>
2015-12-14 15:32:50 +0100

    -  Unnecessary methods have been deleted

--- example-tests/src/test/java/com/epam/restassured/pageobjects/ThankYouPageObject.java
+++ example-tests/src/test/java/com/epam/restassured/pageobjects/ThankYouPageObject.java
@@ -1,15 +1,16 @@
 package com.epam.restassured.pageobjects;
 
 import com.epam.restassured.util.AbstractDriver;
-import com.google.common.base.Verify;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 /**
- * Contains page elements and interaction methods for the test class.
+ * Represents a page object class with page elements, getters and setters.
  *
- * Created by Peter_Olah1 on 12/10/2015.
+ *
+ * @author Peter_Olah1
+ *
  */
 public class ThankYouPageObject extends AbstractDriver {
 
@@ -42,35 +43,4 @@ public class ThankYouPageObject extends AbstractDriver {
         return headerTitle;
     }
 
-    /**
-     * Verifies that is the page sub header containing the user's email address.
-     *
-     * @param firstName The displayed first name.
-     * @param email The displayed email address.
-     */
-    public void whenSubscribeFinishedCheckDataOnPage(String firstName, String email) {
-        Verify.verify(getDriver().getCurrentUrl().startsWith("https://t7-f0x.rhcloud.com/subscription/thank-you.html?"));
-        isHeaderContainsFirstName(firstName);
-        isSubTitleContainsEmail(email);
-    }
-
-    /**
-     * Verifies that is the page header containing the user's first name.
-     *
-     * @param firstName The user's first name.
-     */
-    private void isHeaderContainsFirstName(String firstName) {
-        Verify.verify(headerTitle.getText()
-                .equals("Thank you " + firstName + "!"));
-    }
-
-    /**
-     * Verifies that is the page sub header contains the user's email address.
-     *
-     * @param email The user's email address.
-     *
-     */
-    private void isSubTitleContainsEmail(String email) {
-        Verify.verify(headerSubtitle.getText().contains(email));
-    }
 }
