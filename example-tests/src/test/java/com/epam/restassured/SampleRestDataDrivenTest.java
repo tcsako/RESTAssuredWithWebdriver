ffd5259bb47f81c401b137f1d11ad7ac4c3b3698
Peter Olah1 <peter_olah1@epam.com>
2016-01-07 16:04:01 +0100

    Data Driven Test implementation for SampleRestTest class

---
+++ example-tests/src/test/java/com/epam/restassured/SampleRestDataDrivenTest.java
@@ -0,0 +1,126 @@
+package com.epam.restassured;
+
+import com.epam.restassured.exception.TestExecutionException;
+import com.epam.restassured.pojo.Content;
+import com.epam.restassured.pojo.SubscriberResponse;
+import com.jayway.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class SampleRestDataDrivenTest {
+    private static final int HTTP_OK = HttpStatus.SC_OK;
+
+    private static Logger log = Logger.getLogger(SampleRestDataDrivenTest.class.getName());
+
+    @Parameterized.Parameter
+    public int expectedNumberOfElements;
+
+    @Parameterized.Parameter(value = 1)
+    public int actualNumberOfElements;
+
+    /**
+     *  Sets up the expected and actual parameters for the DDT methods.
+     *
+     * @return A 2 dimension array.
+     */
+    @Parameterized.Parameters
+    public static Collection<Object[]> testDataSet() {
+        log.info("Setting up DDT data");
+        return Arrays.asList(new Object[][] {
+                {1, 1 } ,
+                {1, 1 } ,
+                {1, 1 } ,
+                {1, 1 } ,
+                {1, 1 }
+        });
+    }
+
+    /**
+     *  Sends a delete request to the given path. Deletes the existing entries
+     *  of subsrcibers.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws TestExecutionException {
+        log.info("*************************");
+        log.info("Deleting existing records");
+        if (given().delete(ServiceTestingProperties.REST_API_URL).getStatusCode() == HTTP_OK) {
+            log.info("Records were deleted successfully");
+        } else {
+            log.info("Something went wrong! Existing records couldn't be deleted");
+        }
+    }
+
+    /**
+     *  Gets subsribers data and maps it to the {@link SubscriberResponse} and gets the response as string.
+     *
+     */
+    @Test
+    public void getAllSubscribers() {
+        Response res = get(ServiceTestingProperties.REST_API_URL);
+        SubscriberResponse subscriberResponse = res.as(SubscriberResponse.class);
+        log.info(res.asString());
+        for (Content content : subscriberResponse.getContent()) {
+            log.info(content.getFirstName());
+            log.info(content.getLastName());
+            log.info(content.getEmailAddress());
+            log.info(content.getNewsletterOptIn());
+            log.info(content.getUuid());
+        }
+    }
+
+    /**
+     * Basic verification among the number of existing and the expected records.
+     */
+    @Test
+    @Ignore
+    public void verifyOnlyOneRecord() {
+        // given().authentication().basic("username", "password");
+        when().get(ServiceTestingProperties.REST_API_URL + "?search=John").
+                then().content("numberOfElements", is(expectedNumberOfElements));
+        //TODO felgöngyölíteni az ügyet
+    }
+
+    /**
+     *  Creates a record with the data given in the URL, then gets subscriber data and
+     *  makes basic verifications for assure the correctness of the sent and stored data.
+     *
+     */
+    @Test
+    public void addRecord() {
+        List<String> listToVerifyEmail = new ArrayList<String>();
+        listToVerifyEmail.add("rogermmm@gmail.com");
+        given().contentType("application/json")
+                .post("https://t7-f0x.rhcloud.com/subscription/subscription.html?"
+                        + "firstName=Beluska&lastName=Vagyok&emailAddress=rogermmm@gmail.com&emailAddressConfirmation=rogermmm@gmail.com&newsletterOptIn=true&_newsletterOptIn=on");
+        when().get(ServiceTestingProperties.REST_API_URL + "?search=Beluska").
+                then().content("numberOfElements", is(expectedNumberOfElements)).and()
+                .content("content.emailAddress", equalTo(listToVerifyEmail));
+    }
+
+    /**
+     * Verifies the equality the number of the stored an the expected results.
+     *
+     */
+    @Test
+    public void verifyResultNumber() {
+        assertEquals("Result number should be 1", expectedNumberOfElements, actualNumberOfElements);
+    }
+}
--- example-tests/src/test/java/com/epam/restassured/SampleRestTest.java
+++ example-tests/src/test/java/com/epam/restassured/SampleRestTest.java
@@ -1,18 +1,18 @@
+
 package com.epam.restassured;
 
+import com.epam.restassured.exception.TestExecutionException;
 import com.epam.restassured.pojo.Content;
 import com.epam.restassured.pojo.SubscriberResponse;
+import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import static com.jayway.restassured.RestAssured.*;
@@ -20,122 +20,57 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 
-
-//CSV reader with Singleton implementation
-//AbstractFactory implementation (e.g. PageObject pattern)
-//Subscribe with browser, verify with REST
-//Where to add Delete all
-@RunWith(Parameterized.class)
 public class SampleRestTest {
-    private static Logger log = Logger.getLogger(SampleRestTest.class.getName());
+    private static final int HTTP_OK = HttpStatus.SC_OK;
 
-    @Parameterized.Parameter
-    public int expectedNumberOfElements;
+    private static Logger log = Logger.getLogger(SampleRestDataDrivenTest.class.getName());
 
-    @Parameterized.Parameter(value = 1)
-    public int actualNumberOfElements;
-
-    /**
-     *  Sets up the expected and actual parameters for the DDT methods.
-     *
-     * @return A 2 dimension array.
-     */
-    @Parameterized.Parameters
-    public static Collection<Object[]> testDataSet() {
-        log.info("Setting up DDT data");
-        return Arrays.asList(new Object[][] {
-                {1, 1 } ,
-                {1, 1 } ,
-                {1, 1 } ,
-                {1, 1 } ,
-                {1, 1 }
-        });
-    }
-
-    /**
-     *  Sends a delete request to the given path. Deletes the existing entries
-     *  of subsrcibers.
-     *
-     * @throws Exception
-     */
     @Before
-    public void setUp() throws Exception {
-        log.info("***************");
+    public void setUp() throws TestExecutionException {
+        log.info("*************************");
         log.info("Deleting existing records");
-        given().delete("https://t7-f0x.rhcloud.com/subscription/api/subscribers/");
-    }
-
-    /**
-     *
-     * @throws Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    /**
-     *  Gets subsribers data and maps it to the {@link SubscriberResponse} and gets the response as string.
-     *
-     */
-    @Test
-    public void getAllSubsribers() {
-        Response res = get("https://t7-f0x.rhcloud.com/subscription/api/subscribers/");
-        SubscriberResponse subscriberResponse = res.as(SubscriberResponse.class);
-        log.info("***************");
-        log.info(res.asString());
-        log.info("***************");
-        for (Content content : subscriberResponse.getContent()) {
-            log.info(content.getFirstName());
-            log.info(content.getLastName());
-            log.info(content.getEmailAddress());
-            log.info(content.getNewsletterOptIn());
-            log.info(content.getUuid());
-            log.info("***************");
+        if (given().delete(ServiceTestingProperties.REST_API_URL).getStatusCode() == HTTP_OK) {
+            log.info("Records were deleted successfully");
+        } else {
+            log.info("Something went wrong! Existing records couldn't be deleted");
         }
     }
 
-    /**
-     * Deletes existing subscribers data.
-     */
     @Test
-    public void deleteAllSubscribers() {
-        Response res = delete("https://t7-f0x.rhcloud.com/subscription/api/subscribers/");
-        log.info(res.statusCode());
+    public void getAllSubscribers() {
+        Response res = get(ServiceTestingProperties.REST_API_URL);
+        SubscriberResponse subscriberResponse = res.as(SubscriberResponse.class);
+        System.out.println(res.asString());
+        for (Content content : subscriberResponse.getContent()) {
+            System.out.println(content.getFirstName());
+            System.out.println(content.getLastName());
+            System.out.println(content.getEmailAddress());
+            System.out.println(content.getNewsletterOptIn());
+            System.out.println(content.getUuid());
+        }
     }
 
-    /**
-     * Basic verification among the number of existing and the expected records.
-     */
     @Test
+    @Ignore
     public void verifyOnlyOneRecord() {
-        // given().authentication().basic("username", "password");
+//		given().authentication().basic("username", "password");
         when().get("https://t7-f0x.rhcloud.com/subscription/api/subscribers/?search=John").
-                then().content("numberOfElements", is(expectedNumberOfElements)); //TODO felgöngyölíteni az ügyet
+                then().content("numberOfElements", is(1));
     }
 
-    /**
-     *  Creates a record with the data given in the URL, then gets subscriber data and
-     *  makes basic verifications for assure the correctness of the sent and stored data.
-     *
-     */
     @Test
     public void addRecord() {
-        List<String> list = new ArrayList<String>();
-        list.add("rogermmm@gmail.com");
-        given().contentType("application/json")
-                .post("https://t7-f0x.rhcloud.com/subscription/subscription.html?"
-                        + "firstName=Beluska&lastName=Vagyok&emailAddress=rogermmm@gmail.com&emailAddressConfirmation=rogermmm@gmail.com&newsletterOptIn=true&_newsletterOptIn=on");
-        when().get("https://t7-f0x.rhcloud.com/subscription/api/subscribers/?search=Beluska").
-                then().content("numberOfElements", is(expectedNumberOfElements)).and()
-                .content("content.emailAddress", equalTo(list));
+        List<String> listToVerifyEmail = new ArrayList<String>();
+        listToVerifyEmail.add("rogermmm@gmail.com");
+        given().contentType("application/json").post("https://t7-f0x.rhcloud.com/subscription/subscription.html?firstName=Beluska&lastName=Vagyok&emailAddress=rogermmm@gmail.com&emailAddressConfirmation=rogermmm@gmail.com&newsletterOptIn=true&_newsletterOptIn=on");
+        when().get(ServiceTestingProperties.REST_API_URL + "?search=Beluska").
+                then().content("numberOfElements", is(1)).and().content("content.emailAddress", equalTo(listToVerifyEmail));
     }
 
-    /**
-     * Verifies the equality the number of the stored an the expected results.
-     *
-     */
     @Test
     public void verifyResultNumber() {
-        assertEquals("Result number should be 1", expectedNumberOfElements, actualNumberOfElements);
+        Response res = get("https://t7-f0x.rhcloud.com/subscription/api/subscribers/?search=John");
+        JsonPath jp = new JsonPath(res.asString());
+        assertEquals("Result number should be 1", 1, jp.getInt("numberOfElements"));
     }
 }
