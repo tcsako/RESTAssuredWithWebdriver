5b33946a7dfc47999cde6d5c72ffccc8380ec7e7
Peter Olah1 <peter_olah1@epam.com>
2016-01-07 18:01:06 +0100

    No functional changes. Only formatted for better readability

--- example-tests/src/test/java/com/epam/restassured/pojo/SubscriberResponse.java
+++ example-tests/src/test/java/com/epam/restassured/pojo/SubscriberResponse.java
@@ -3,67 +3,85 @@ package com.epam.restassured.pojo;
 import java.util.List;
 
 public class SubscriberResponse {
-	private List<Content> content;
-	private String last;
-	private String totalPages;
-	private String totalElements;
-	private String first;
-	private String sort;
-	private String numberOfElements;
-	private String size;
-	private String number;
-	public List<Content> getContent() {
-		return content;
-	}
-	public void setContent(List<Content> content) {
-		this.content = content;
-	}
-	public String getLast() {
-		return last;
-	}
-	public void setLast(String last) {
-		this.last = last;
-	}
-	public String getTotalPages() {
-		return totalPages;
-	}
-	public void setTotalPages(String totalPages) {
-		this.totalPages = totalPages;
-	}
-	public String getTotalElements() {
-		return totalElements;
-	}
-	public void setTotalElements(String totalElements) {
-		this.totalElements = totalElements;
-	}
-	public String getFirst() {
-		return first;
-	}
-	public void setFirst(String first) {
-		this.first = first;
-	}
-	public String getSort() {
-		return sort;
-	}
-	public void setSort(String sort) {
-		this.sort = sort;
-	}
-	public String getNumberOfElements() {
-		return numberOfElements;
-	}
-	public void setNumberOfElements(String numberOfElements) {
-		this.numberOfElements = numberOfElements;
-	}
-	public String getSize() {
-		return size;
-	}
-	public void setSize(String size) {
-		this.size = size;
-	}
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
-	}
+    private List<Content> content;
+    private String last;
+    private String totalPages;
+    private String totalElements;
+    private String first;
+    private String sort;
+    private String numberOfElements;
+    private String size;
+    private String number;
+
+    public List<Content> getContent() {
+        return content;
+    }
+
+    public void setContent(List<Content> content) {
+        this.content = content;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(String totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public String getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(String totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public void setNumberOfElements(String numberOfElements) {
+        this.numberOfElements = numberOfElements;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
