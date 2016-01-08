2e66afbc510b52fd6566fbd0581fc1d4a9927a41
Jozsef Koza <jozsef.koza@gmail.com>
2016-01-08 09:45:01 +0100

    Clean test

--- example-tests/src/test/java/com/epam/restassured/BasicServiceTestWithRest.java
+++ example-tests/src/test/java/com/epam/restassured/BasicServiceTestWithRest.java
@@ -7,41 +7,30 @@ package com.epam.restassured;
 //TODO: 5. create DDT script for REST script
 //TODO: 6. create DDT script for webdriver script
 //TODO: 7. create rest script without BDD style (using JUnit asssertions)
-import com.epam.restassured.pageobjects.SignUpPagePageObject;
-import com.epam.restassured.pageobjects.ThankYouPagePageObject;
-import com.epam.restassured.pageobjects.ThankYouPageVerifier;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-
 public class BasicServiceTestWithRest {
-    private static Logger log = Logger.getLogger(BasicServiceTestWithRest.class);
+    private static final Logger LOG = Logger.getLogger(BasicServiceTestWithRest.class);
+
     private static final int NUMBER_OF_RESPONSE = 1;
     private static final String CONTENT_NUMBER_OF_ELEMENTS = "numberOfElements";
     private static final String CONTENT_EMAIL_ADDRESS = "content.emailAddress";
 
-    // Test data
-    private String firstName;
-    private String lastName;
-    private String emailAddress;
-    private String emailAddressConfirmation;
-    private String newsletterOptIn;
-    private int httpOkStatus;
-    // Verification
-    private List<String> listToVerifyEmail;
-
-    private SignUpPagePageObject signUpPagePageObject;
-    private ThankYouPagePageObject thankYouPagePageObject;
-    private ThankYouPageVerifier thankYouPageVerifier;
+    private static final String FIRST_NAME = "John";
+    private static final String LAST_NAME = "Doe";
+    private static final String EMAIL_ADDRESS = "johndoe@freecloud.com";
+    private static final String EMAIL_ADDRESS_CONFIRMATION = "johndoe@freecloud.com";
+    private static final String NEWSLETTER_OPT_IN = "true";
 
     /**
      * Responsible for setting up test data and environment.
@@ -50,28 +39,12 @@ public class BasicServiceTestWithRest {
      */
     @Before
     public void setUp() throws Exception {
-
-        log.info("*******************************************");
-        log.info("Deleting existing records");
-        if (given().delete(ServiceTestingProperties.REST_API_URL).getStatusCode() == httpOkStatus) {
-            log.info("Records were deleted successfully");
+        LOG.info("Deleting existing records");
+        if (given().delete(ServiceTestingProperties.REST_API_URL).getStatusCode() == HttpStatus.SC_OK) {
+            LOG.info("Records were deleted successfully");
         } else {
-            log.info("Something went wrong! Existing records couldn't be deleted");
+            LOG.info("Something went wrong! Existing records couldn't be deleted");
         }
-
-        log.info("Setting up test data");
-        firstName = "John";
-        lastName = "Doe";
-        emailAddress = "johndoe@freecloud.com";
-        emailAddressConfirmation = "johndoe@freecloud.com";
-        httpOkStatus =  HttpStatus.SC_OK;
-        newsletterOptIn = "true";
-
-        log.info("Setting up verification data");
-        listToVerifyEmail = new ArrayList<String>();
-        listToVerifyEmail.add(emailAddress);
-        log.info("Initializing Firefox driver");
-        log.info("Opening subscription page");
     }
 
     /**
@@ -82,10 +55,10 @@ public class BasicServiceTestWithRest {
     @Test
     public void addRecord() {
         given().contentType(ServiceTestingProperties.JSON_CONTENT_TYPE).
-        and().post(ServiceTestingProperties.getUrlToPostData(firstName, lastName, emailAddress, emailAddressConfirmation, newsletterOptIn));
+                and().post(ServiceTestingProperties.getUrlToPostData(FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, EMAIL_ADDRESS_CONFIRMATION, NEWSLETTER_OPT_IN));
         when().get(ServiceTestingProperties.REST_API_URL).
-        then().statusCode(httpOkStatus).
-        and().content(CONTENT_NUMBER_OF_ELEMENTS, is(NUMBER_OF_RESPONSE)).
-        and().content(CONTENT_EMAIL_ADDRESS, equalTo(listToVerifyEmail));
+                then().statusCode(HttpStatus.SC_OK).
+                and().content(CONTENT_NUMBER_OF_ELEMENTS, is(equalTo(NUMBER_OF_RESPONSE))).
+                and().content(CONTENT_EMAIL_ADDRESS, contains(EMAIL_ADDRESS));
     }
 }
